20a69a076ccd1b43eeb689d48e293885b7404e23
unknown <Richard.Dorta@iam.local>
2018-09-26 15:20:31 -0500

    Stated Movement
    Got the very basics on player movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+ 
+    {
+        [SerializeField]
+        private int Lives = 5;
 
-	// Use this for initialization
-	void Start () {
-		
+        [SerializeField]
+        private string Name = "Mario";
+    
+        [SeralizeField]
+        private float JumpHieght = 5, Speed = 5;
+
+        private bool HasKey;
+
+        private bool IsOnGround;
+
+        private RigidBody2D RigidBody2DInstance;
+        
+        private float HorizontalInput;
+
+	    // Use this for initialization
+	    void Start ()
+        {
+            // Have to initilize our Rigid Body value
+            RigidBody2DInstance = GetCompnent<RigidBody2D>();
+              
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        //This doesn;t matter. Delete when you can
-		
-	}
-}
+	private void Update ()
+    {
+        GetInput();
+        Move();
+    }
+
+    private void GetInput()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+    }
+    private void Move()
+    {
+        RigidBody2DInstance.velocity = new Vector2(HorizontalInput, 0);
+    }
+
